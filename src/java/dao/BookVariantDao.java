4ee0c3da7b5074bac3f863b28e0365d286cbd2ae
mimosana <nguyenhongnhung250705@gmail.com>
2025-12-19 01:22:26 +0700

    update-variant2

--- src/java/dao/BookVariantDao.java
+++ src/java/dao/BookVariantDao.java
@@ -11,7 +11,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import model.Book;
 import model.BookVariant;
+import model.User;
 
 /**
  *
